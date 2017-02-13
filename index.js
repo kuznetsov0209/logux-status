f39ec39c43d52ba063cfa9cf4ca24dc452fa494d
Andrey Sitnik <andrey@sitnik.ru>
2017-02-13 10:33:41 +0400

    Clean up code

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ var attention = require('./attention')
 module.exports = {
   attention: attention
 }
+
+/**
+ * Any object with Sync instance in `sync` property.
+ * For example, `logux-client` instance.
+ *
+ * @typedef {object} Syncable
+ * @property {BaseSync} sync The sync instance.
+ */
